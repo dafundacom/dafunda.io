75c41cd7fb0b522c346a7349286d6425311059ce
Karyana Yandi <karyana@lokan.id>
2022-01-29 09:04:35 +0700

    ci: add WordPress Redirect Theme for Headless to project page

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,6 +6,13 @@ const projectsData = [
     href: "https://wowkia.com",
     cta: "See Demo",
   },
+  {
+    title: "WordPress Redirect Theme for Headless",
+    description: `Redirect WordPress Blog to Headless CMS or another site.`,
+    imgSrc: "/static/images/redirect-theme-for-headless.png",
+    href: "https://github.com/dafundacom/redirect-theme-for-headless",
+    cta: "Go to Repository",
+  },
 ]
 
 export default projectsData
